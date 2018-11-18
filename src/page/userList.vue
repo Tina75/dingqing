6a864561bbdddc198befc3ed19301966c441d6d9
Tina75 <1009104091@qq.com>
2018-11-18 16:07:19 +0800

    权限

--- src/page/userList.vue
+++ src/page/userList.vue
@@ -15,6 +15,35 @@
         border
         :data="tableData"
         style="width: 100%">
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <div class="table-inner">
+              <div class="head">
+                <div class="item">文件类型</div>
+                <div class="item">权限</div>
+              </div>
+              <div v-for="item in props.row.permissions">
+                <div class="body" v-if="JSON.parse(item)[1]">
+                  <div class="item">自拟文件</div>
+                  <div class="item">
+                    <el-checkbox-group v-model="JSON.parse(item)[1]" @change="_modifyUser(props.row)">
+                      <el-checkbox label="import">导入</el-checkbox>
+                      <el-checkbox label="export">导出</el-checkbox>
+                      <el-checkbox label="insert" >新增</el-checkbox>
+                      <el-checkbox label="delete"> 删除</el-checkbox>
+                      <el-checkbox label="update">修改</el-checkbox>
+                      <el-checkbox label="view">查看</el-checkbox>
+                    </el-checkbox-group>
+                  </div>
+                </div>
+                <div class="item" v-if="JSON.parse(item)[2]">内部文件</div>
+                <div class="item" v-if="JSON.parse(item)[3]">机要文件</div>
+                <div class="item" v-if="JSON.parse(item)[4]">密码文件</div>
+                <div class="item" v-if="JSON.parse(item)[5]">县级文件</div>
+              </div>
+            </div>
+          </template>
+        </el-table-column>
         <el-table-column
           prop="user.id"
           label="ID"
@@ -33,6 +62,7 @@
           label="权限">
           <template slot-scope="scope">
             <!--<span v-for="item in scope.row.permissions">{{item}}</span>-->
+            <div>scope.row.permissions</div>
             <el-checkbox-group v-model="scope.row.permissions" @change="_modifyUser(scope.row)">
               <el-checkbox label="insert" >新增</el-checkbox>
               <el-checkbox label="delete"> 删除</el-checkbox>
@@ -143,6 +173,12 @@
   }
 </style>
 <style scoped lang="stylus">
+  .table-inner
+    .head,.body
+      display flex
+      .item
+        flex 1
+        padding: 12px 0
   .detail-container
     .search
       .concatField

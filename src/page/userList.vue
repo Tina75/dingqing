cc30d4e7ac5524f53b4014452a824d53aa6e9c4c
Tina75 <1009104091@qq.com>
2018-12-02 14:04:05 +0800

    权限增加

--- src/page/userList.vue
+++ src/page/userList.vue
@@ -22,11 +22,11 @@
                 <div class="item">文件类型</div>
                 <div class="item">权限</div>
               </div>
-              <div v-for="item in props.row.permissions">
-                <div class="body" v-if="JSON.parse(item)[1]">
+              <div v-for="(item, index) in props.row.permissions" :key="index">
+                <div class="body" v-if="item[1]">
                   <div class="item">自拟文件</div>
                   <div class="item">
-                    <el-checkbox-group v-model="JSON.parse(item)[1]" @change="_modifyUser(props.row)">
+                    <el-checkbox-group v-model="props.row.permissions[index][1]"  @change="_modifyUser(props.row)">
                       <el-checkbox label="import">导入</el-checkbox>
                       <el-checkbox label="export">导出</el-checkbox>
                       <el-checkbox label="insert" >新增</el-checkbox>
@@ -36,10 +36,58 @@
                     </el-checkbox-group>
                   </div>
                 </div>
-                <div class="item" v-if="JSON.parse(item)[2]">内部文件</div>
-                <div class="item" v-if="JSON.parse(item)[3]">机要文件</div>
-                <div class="item" v-if="JSON.parse(item)[4]">密码文件</div>
-                <div class="item" v-if="JSON.parse(item)[5]">县级文件</div>
+                <div class="body" v-if="item[2]">
+                  <div class="item">内部文件</div>
+                  <div class="item">
+                    <el-checkbox-group v-model="props.row.permissions[index][2]"  @change="_modifyUser(props.row)">
+                      <el-checkbox label="import">导入</el-checkbox>
+                      <el-checkbox label="export">导出</el-checkbox>
+                      <el-checkbox label="insert" >新增</el-checkbox>
+                      <el-checkbox label="delete"> 删除</el-checkbox>
+                      <el-checkbox label="update">修改</el-checkbox>
+                      <el-checkbox label="view">查看</el-checkbox>
+                    </el-checkbox-group>
+                  </div>
+                </div>
+                <div class="body" v-if="item[3]">
+                  <div class="item">机要文件</div>
+                  <div class="item">
+                    <el-checkbox-group v-model="props.row.permissions[index][3]"  @change="_modifyUser(props.row)">
+                      <el-checkbox label="import">导入</el-checkbox>
+                      <el-checkbox label="export">导出</el-checkbox>
+                      <el-checkbox label="insert" >新增</el-checkbox>
+                      <el-checkbox label="delete"> 删除</el-checkbox>
+                      <el-checkbox label="update">修改</el-checkbox>
+                      <el-checkbox label="view">查看</el-checkbox>
+                    </el-checkbox-group>
+                  </div>
+                </div>
+                <div class="body" v-if="item[4]">
+                  <div class="item">密码文件</div>
+                  <div class="item">
+                    <el-checkbox-group v-model="props.row.permissions[index][4]"  @change="_modifyUser(props.row)">
+                      <el-checkbox label="import">导入</el-checkbox>
+                      <el-checkbox label="export">导出</el-checkbox>
+                      <el-checkbox label="insert" >新增</el-checkbox>
+                      <el-checkbox label="delete"> 删除</el-checkbox>
+                      <el-checkbox label="update">修改</el-checkbox>
+                      <el-checkbox label="view">查看</el-checkbox>
+                    </el-checkbox-group>
+                  </div>
+                </div>
+                <div class="body" v-if="item[5]">
+                  <div class="item">县级文件</div>
+                  <div class="item">
+                    <el-checkbox-group v-model="props.row.permissions[index][5]"  @change="_modifyUser(props.row)">
+                      <el-checkbox label="import">导入</el-checkbox>
+                      <el-checkbox label="export">导出</el-checkbox>
+                      <el-checkbox label="insert" >新增</el-checkbox>
+                      <el-checkbox label="delete"> 删除</el-checkbox>
+                      <el-checkbox label="update">修改</el-checkbox>
+                      <el-checkbox label="view">查看</el-checkbox>
+                    </el-checkbox-group>
+                  </div>
+                </div>
               </div>
             </div>
           </template>
@@ -58,22 +106,22 @@
           prop="user.password"
           label="密码">
         </el-table-column>
-        <el-table-column
-          label="权限">
-          <template slot-scope="scope">
-            <!--<span v-for="item in scope.row.permissions">{{item}}</span>-->
-            <div>scope.row.permissions</div>
-            <el-checkbox-group v-model="scope.row.permissions" @change="_modifyUser(scope.row)">
-              <el-checkbox label="insert" >新增</el-checkbox>
-              <el-checkbox label="delete"> 删除</el-checkbox>
-              <el-checkbox label="update">更新</el-checkbox>
-            </el-checkbox-group>
-          </template>
-        </el-table-column>
+        <!--<el-table-column-->
+          <!--label="权限">-->
+          <!--<template slot-scope="scope">-->
+            <!--&lt;!&ndash;<span v-for="item in scope.row.permissions">{{item}}</span>&ndash;&gt;-->
+            <!--<div>scope.row.permissions</div>-->
+            <!--<el-checkbox-group v-model="scope.row.permissions" @change="_modifyUser(scope.row)">-->
+              <!--<el-checkbox label="insert" >新增</el-checkbox>-->
+              <!--<el-checkbox label="delete"> 删除</el-checkbox>-->
+              <!--<el-checkbox label="update">更新</el-checkbox>-->
+            <!--</el-checkbox-group>-->
+          <!--</template>-->
+        <!--</el-table-column>-->
         <el-table-column
           label="操作"
           fixed="right"
-        width="100">
+        >
           <template slot-scope="scope">
             <el-button @click="modifyClick(scope.row)" type="warning" size="small">修改</el-button>
           </template>
@@ -126,12 +174,21 @@
       _search () {
         search(this.id).then(res => {
           if (res.data.status === config.ERR_OK) {
-            this.tableData = res.data.data
+            let data = res.data.data
+            for (let i = 0; i < data.length; i++) {
+              data[i].permissions = data[i].permissions.map(item => {
+                return JSON.parse(item)
+              })
+            }
+            console.log(data)
+            this.tableData = data
           }
         })
       },
       _modifyUser (row) {
-        console.log(row)
+        row.permissions = row.permissions.map(item => {
+          return JSON.stringify(item)
+        })
         if (row) {
           this.modifyDate = {
             username: row.user.username,
@@ -154,7 +211,11 @@
         })
       },
       modifyClick (row) {
+        console.log(row)
         this.modifyShow = true
+        row.permissions = row.permissions.map(item => {
+          return JSON.stringify(item)
+        })
         this.modifyDate = {
           username: row.user.username,
           password: row.user.password,
@@ -176,6 +237,7 @@
   .table-inner
     .head,.body
       display flex
+      /*border-bottom 1px solid #ddd*/
       .item
         flex 1
         padding: 12px 0
